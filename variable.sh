01a6a17ea22924da7df7c61a377a2bbcd6e82a5a
venkat-83 <venkatasaigurla83@gmail.com>
2026-06-30 11:54:37 +0530

    Delete variable.sh

--- variable.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Trump:hey iran , we are coming"
-echo "iran: yes, please come"
-echo "Trumo: hey iran i am serious"
-echo "iran: hey trump, we are also serious"
-echo "Trump: i am ready to coming"
-echo "iran: yaa please come"
